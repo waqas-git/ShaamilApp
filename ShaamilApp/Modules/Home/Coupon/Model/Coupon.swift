ed0240e5f0f83fe39075893ebea8a71670fbb70b
Waqas Ahmed <waqasahmed.uol@gmail.com>
2025-04-09 00:49:47 +0500

    add new model in coupon module

---
+++ ShaamilApp/Modules/Home/Coupon/Model/Coupon.swift
@@ -0,0 +1,11 @@
+//
+//  Coupon.swift
+//  ShaamilApp
+//
+//  Created by waqas ahmed on 09/04/2025.
+//
+
+import Foundation
+struct Coupon{
+    
+}
---
+++ ShaamilApp/Modules/Home/Deals/DealsViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  DealsViewModel.swift
+//  ShaamilApp
+//
+//  Created by waqas ahmed on 09/04/2025.
+//
+
+import SwiftUI
+class DealsViewModel: ObservableObject{
+    @Published var dealsItems: String = ""
+}
